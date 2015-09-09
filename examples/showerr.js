ce1098afa9d0b164939674c3fb3c97992d32756a
Cheng Kuan <lab@strllar.org>
2015-09-10 00:29:06 +0800

    enchance showerr, support txresult in txhistory table

--- examples/showerr.js
+++ examples/showerr.js
@@ -3,5 +3,20 @@ var util = require('util');
 
 let buf = new Buffer(process.argv[2], "base64")
 console.log(buf)
-let err = StellarBase.xdr.TransactionResult.fromXDR(buf)
-console.log(util.inspect(err, false, null))
+
+try {
+	let err = StellarBase.xdr.TransactionResult.fromXDR(buf)
+	console.log("TransactionResult")
+	console.log(util.inspect(err, false, null))
+}
+catch(e) {
+}
+
+try {
+	let err = StellarBase.xdr.TransactionResultPair.fromXDR(buf)
+		console.log("TransactionResultPair")
+	console.log(util.inspect(err, false, null))
+}
+catch(e) {
+}
+
